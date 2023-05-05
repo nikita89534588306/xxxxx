53d2554ec10d80a9be858233587dec166b138a36
Nikita Balashov <noWorkingEmail@mail.ru>
2023-05-05 17:02:03 +0500

    xxxx

--- app.js
+++
@@ -1,6 +0,0 @@
-    //получаем все ссылки в документе
-    var contacts = document.querySelectorAll('.contact');
-    //перебираем элементы массива
-    for(let thisContact of contacts) 
-        //добавляем обработчик нажатия на ссылку правой кнопкой мыши
-        thisContact.oncontextmenu = handlerContextMenu
---
+++ js/contactContextMenu.js
@@ -0,0 +1,71 @@
+class ContextMenu{
+    constructor(contact){
+
+        this.INDEX = 1;
+        this.path = contact.parentElement.children[this.INDEX];
+    }
+
+    get isOpen (){ return this.path.className.split(' ').includes("show")  }
+
+    open = function(){  
+
+        this.path.classList.add("show")
+
+        window.addEventListener('click', function closeContextMenuContact(){
+
+            contactList.closeContextMenuContacts()
+            this.window.removeEventListener( 'click',closeContextMenuContact )
+
+        })
+    }
+
+    close = function(){  this.path.classList.remove("show") }
+
+}
+class Contact
+{
+    contextMenu;
+
+    constructor(contact){
+
+       this.contextMenu = new ContextMenu(contact);
+       contact.oncontextmenu = this.callContextMenu
+       
+    }
+
+    callContextMenu = function()
+    {
+        event.preventDefault();
+        let contextMenu = new ContextMenu(this);
+        contextMenu.open()
+    }
+
+}
+
+class ContactsList{
+    contact = []
+    constructor( contactsHTMLCollection ) {
+        
+        for(let oneContactHTML of contactsHTMLCollection )
+        {    
+            this.contact.push( new Contact( oneContactHTML ) )   
+        }   
+
+    }
+    closeContextMenuContacts = function(){
+
+        for (let contact of this.contact){   
+            
+            if(contact.contextMenu.isOpen){ 
+               
+                contact.contextMenu.close(); 
+                break 
+
+            }
+        }
+    }
+}
+
+
+
+let contactList = new ContactsList( document.querySelectorAll('.contact') );
